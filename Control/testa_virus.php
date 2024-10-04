819c6d9d6a0a8550080119a5920489033f82dace
FelipeMenezes937 <145990627+FelipeMenezes937@users.noreply.github.com>
2024-10-04 10:26:13 -0300

    bug da pag arquivo resolvido

--- Control/testa_virus.php
+++ Control/testa_virus.php
@@ -5,7 +5,6 @@ use GuzzleHttp\Client;
 header("Content-Type: text/plain"); 
 
 $virustotal_api_key = "9c786184ee0b07f3f16436272314583f89cc76a011add8d9feeb75b0bc41600e";
-
 $file_to_scan =  $_FILES['arquivo']['tmp_name'];
 
 $file_size_mb = filesize($file_to_scan)/1024/1024;
@@ -21,7 +20,7 @@ $api_reply = $response->getBody()->getContents();
 $api_reply_array = json_decode($api_reply, true);
 
 if($api_reply_array['response_code'] == -2){
-    echo $api_reply_array['verbose_msg'];
+    $resultado = $api_reply_array['verbose_msg'];
 }
 
 if ($api_reply_array['response_code'] == 1) {
@@ -34,7 +33,7 @@ if ($api_reply_array['response_code'] == 1) {
 
 // [PART 2] se o arquivo n for encontrado a gnt manda o arquivo para relatório
 if($api_reply_array['response_code'] == 0){
-
+    $resultado = "o arquivo não consta em nenhum banco, <strong> é seguro </strong>";
     $post_url = 'https://www.virustotal.com/vtapi/v2/file/scan';
 
     if($file_size_mb >= 32){
--- View/detalhes.php
+++ View/detalhes.php
@@ -1,3 +1,23 @@
+<?php
+print_r($_COOKIE);
+
+if (isset($_COOKIE['relatorio'])) {
+    $relatorio = unserialize($_COOKIE['relatorio']);
+    
+   
+    print_r($relatorio);
+    
+    echo "<h2>Relatório dos Antivírus</h2>";
+    foreach ($relatorio as $banco => $dados) {
+        echo "<strong>Banco:</strong> " . htmlspecialchars($banco, ENT_QUOTES, 'UTF-8') . "<br>";
+        echo "<strong>Resultado:</strong> " . htmlspecialchars($dados['result'], ENT_QUOTES, 'UTF-8') . "<br><br>";
+    }
+} else {
+    echo "Nenhum resultado disponível.";
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,23 +28,8 @@
 <body>
     <?php
  // Adicione uma verificação para ver todos os cookies disponíveis
-print_r($_COOKIE);
 
-if (isset($_COOKIE['relatorio'])) {
-    $relatorio = unserialize($_COOKIE['relatorio']);
     
-    // Verifique o conteúdo do array $relatorio após desserializar
-    print_r($relatorio);
-    
-    // Exibindo os dados do array $relatorio
-    echo "<h2>Relatório dos Antivírus</h2>";
-    foreach ($relatorio as $banco => $dados) {
-        echo "<strong>Banco:</strong> " . htmlspecialchars($banco, ENT_QUOTES, 'UTF-8') . "<br>";
-        echo "<strong>Resultado:</strong> " . htmlspecialchars($dados['result'], ENT_QUOTES, 'UTF-8') . "<br><br>";
-    }
-} else {
-    echo "Nenhum resultado disponível.";
-}
 
     ?>
 </body>
